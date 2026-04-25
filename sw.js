684cf8a256c1d46997844011373cbc56450bdd11
JB De <deguellejb@gmail.com>
2026-04-25 18:31:03 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'f3f-manager-v6.3';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './manifest.json'
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Activation : Nettoyage des anciens caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+});
+
+// Fetch : Répondre avec le cache si hors-ligne
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
